cc8a85e6f3fc557ed760cd294877d1d3ecba5904
PlubCPE <chakkapop_s@cmu.ac.th>
2022-08-04 14:14:54 +0700

    1st

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const btnRandom = document.getElementById("btn-random");
+const Pname = document.getElementById("p-name");
+const Paddress = document.getElementById("p-address");
+const Pemail = document.getElementById("p-email");
+const imgProfile = document.queryCommandIndeterm("#img-profile");
+
+Start();
+
+function Start() {
+  callApi();
+  loadingCard.display.style = "none";
+}
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  imgProfile.src = resp.data.result;
+  const data = resp.data.result[0];
+  resp.onload = () => {
+    loadingCard.style;
+  };
 }
+
+btnRandom.onclick = async () => {
+  btnRandom.innerText = "Loading...";
+  btnRandom.disabled = true;
+  callApi();
+};
