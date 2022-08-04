22f8f0e60aded2198919bd38803afc4f87ffa138
PlubCPE <chakkapop_s@cmu.ac.th>
2022-08-04 14:25:44 +0700

    2nd

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
-const btnRandom = document.getElementById("btn-random");
-const Pname = document.getElementById("p-name");
-const Paddress = document.getElementById("p-address");
-const Pemail = document.getElementById("p-email");
-const imgProfile = document.queryCommandIndeterm("#img-profile");
+const img = document.querySelector("#img-profile");
+const loadingProfile = document.querySelector("#div-loading-card");
+const names = document.querySelector("#p-name");
+const email = document.querySelector("#p-email");
+const address = document.querySelector("#p-address");
+const generate = document.querySelector("#btn-random");
+const emoji = document.querySelector("#span-gender-icon");
 
-Start();
+start();
 
-function Start() {
+function start() {
   callApi();
-  loadingCard.display.style = "none";
+  loadingProfile.style.display = "";
 }
 
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
-  imgProfile.src = resp.data.result;
-  const data = resp.data.result[0];
-  resp.onload = () => {
-    loadingCard.style;
-  };
+  const data = resp.data.results[0];
+
+  img.src = data.picture.large;
+  names.innerHTML = `${data.name.first} ${data.name.last}`;
+  address.innerHTML = `${data.location.city}, ${data.location.state}`;
+  email.innerHTML = data.email;
+  if (data.gender == "female") {
+    emoji.innerText = "👧";
+  } else {
+    emoji.innerText = "👨";
+  }
 }
 
-btnRandom.onclick = async () => {
-  btnRandom.innerText = "Loading...";
-  btnRandom.disabled = true;
-  callApi();
+generate.onclick = () => {
+  start();
+};
+
+img.onload = () => {
+  loadingProfile.style.display = "none";
+
+  generate.style.display = "none";
 };
